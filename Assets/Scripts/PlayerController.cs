7e60647096ee6914639c6dd1ffa96af6e9ea58fa
NAAP120 <naap.sv120@gmail.com>
2025-12-07 17:24:04 -0600

    Added idel y Run states

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,7 +7,11 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D m_rigidbody2D;
     private GatherInput m_gatherImput;
     private Transform m_transform;
+    private Animator m_animator; 
+
     [SerializeField] private float speed;
+    private int direction = 1;
+    private int IdSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,11 +19,35 @@ public class PlayerController : MonoBehaviour
         m_gatherImput = GetComponent<GatherInput>();
         m_transform = GetComponent<Transform>();
         m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+        IdSpeed = Animator.StringToHash("Speed");
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        SetAnimatorValues();
+    }
+    private void SetAnimatorValues()
+    {
+         m_animator.SetFloat(IdSpeed, MathF.Abs(m_rigidbody2D.linearVelocityX));
+    }
     void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        Flip();
         m_rigidbody2D.linearVelocity = new Vector2(speed * m_gatherImput.valueX, m_rigidbody2D.linearVelocityY);
+    }
+
+    private void Flip()
+    {
+        if(m_gatherImput.valueX * direction < 0)
+        {
+            m_transform.localScale = new Vector3(-m_transform.localScale.x, 1, 1);
+            direction *= -1;
+        }
     }
 }
